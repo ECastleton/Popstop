fe6e2011c8c17960777fa2dccc244b8d367c7366
ECastleton <Emilee.Castleton@gmail.com>
2017-04-28 11:16:50 -0600

    static time entered in weekly calendar will change next week

--- popsicle/weeklyCal/views.py
+++ popsicle/weeklyCal/views.py
@@ -32,3 +32,4 @@ def list_events(request, times=TimeSlot.objects.all):
             week_events[DAYS[i]]["locationName"] = location.name
             week_events[DAYS[i]]["address"] = str(location.address).replace(" ","+")
     return render(request, "weeklyCal/calendar.html", week_events)
+
